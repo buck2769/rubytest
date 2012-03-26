d97019ff4be9be2ccb6e336d8a0204406ec78df8
Evil Buck <buck2769@gmail.com>
2012-03-25 21:47:08 -0400

    step 2

--- test_calculator.rb
+++ test_calculator.rb
@@ -6,31 +6,20 @@ class TestAdd < Test::Unit::TestCase
     @calc = Calculator.new
   end
   
-  def test_initial_total
-    
-    assert_equal @calc.total, 0
-  end
-  
   def test_empty_string
-    @calc.add("")
-    
-    assert_equal 0, @calc.total
-  end
-  
-  def test_add_one_number
-    @calc.add("3")
-    assert_equal 3, @calc.total
-    
-    @calc.add("4")
-    assert_equal 7, @calc.total
+    assert_equal 0, @calc.Add("")
   end
   
   def test_add
-    @calc.add("1,2")
-    assert_equal 3, @calc.total
-    
-    @calc.add("7, 3")
-    assert_equal 13, @calc.total
-    
+    assert_equal 7, @calc.Add("2,5")
+  end
+  
+  def test_multiple_digit_numbers
+    assert_equal 20, @calc.Add("12,8")
+    assert_equal 1200, @calc.Add("348,852")
+  end
+  
+  def test_2_or_more_numbers
+    assert_equal 165, @calc.Add("1,3,5,56,100")
   end
 end
